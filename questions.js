6668f355a1f8a114a5acd9becc8fb966345882fa
picaminc <picaminc@gmail.com>
2022-07-31 23:37:17 +0900

    quiz-app files

---
+++ questions.js
@@ -0,0 +1,29 @@
+//点数用の変数
+let score = 0;
+
+const form = document.querySelector('#questions');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const data = new FormData(e.currentTarget);
+
+  const correctAnswers = {
+    question1: 'B',
+    question2: 'A',
+    question3: 'B',
+  };
+ 
+  // 答えは正解か不正解か確認
+  for (var pair of data.entries()) {
+    const isCorrect = correctAnswers[pair[0]] === pair[1];
+
+    // 正解の答えの計算
+    if (isCorrect) {
+      score++;
+    }
+  }
+  
+  // 点数を反映する
+  alert('あなたの点数は: ' + score + '点です！');
+});
+
